63ceb78c6ca97175ab3ac5c00041b8c9446e9c69
Camrynne Sullivan <camrynne.sullivan@gmail.com>
2020-07-07 19:49:26 -0400

    Second Version HTML, CSS, JS

--- quizcode.js
+++ quizcode.js
@@ -1,3 +1,57 @@
+//create an array with an actual quiz =====================================================
+
+var quiz = [
+  // question 1
+  {
+    q1: "Who wrote CATS the Musical?",
+    a: "Stephen Sondheim",
+    b: "Celine Dion",
+    c: "Prince",
+    d: "Andrew Lloyd Weber",
+  },
+  // question 2
+  {
+    q2: "CATS is based on a poem by...",
+    a: "Emily Dickinson",
+    b: "Emerson",
+    c: "T.S. Eliott",
+    d: "Tennessee Williams",
+  },
+
+  // question 3
+  {
+    q3: "What is a jellicle cat?",
+    a: "any cute beloved cat",
+    b: "alley cat",
+    c: "forest cat",
+    d: "mountain cat",
+  },
+];
+
+//DOM Elements ===========================================================================
+
+var timerEl = document.querySelector(".timer");
+var startEl = document.querySelector("#start");
+
+//Click Start
+
+// var timeEl = document.querySelector(".time");
+// var mainEl = document.getElementById("main");
+
+// var secondsLeft = 10;
+
+// function setTime() {
+//   var timerInterval = setInterval(function () {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+//     if (secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//       sendMessage();
+//     }
+//   }, 1000);
+// }
+// ===========================================================================
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
@@ -10,16 +64,18 @@
 // WHEN the game is over
 // THEN I can save my initials and score
 
+//====================================================================================
 // Predetermined Data example- Set everything to zero
 // score counter
 // timer
 // shit ton of variables
 // objects (are the mulitple choice answers?)
 
-
 // Quiz Elements
 
 // Click Start button
+
+// start.addEventListener("click", startQuiz);
 
 //Then 2min timer starts
 
