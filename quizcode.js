94b30f52211f22c7554884251d21680d9e6cca96
Camrynne Sullivan <camrynne.sullivan@gmail.com>
2020-07-06 22:21:47 -0400

    Linked Bootstrap CSS Version 1

---
+++ quizcode.js
@@ -0,0 +1,40 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+// Predetermined Data example- Set everything to zero
+// score counter
+// timer
+// shit ton of variables
+// objects (are the mulitple choice answers?)
+
+
+// Quiz Elements
+
+// Click Start button
+
+//Then 2min timer starts
+
+//First question pops up
+
+//User Input Question one
+
+//if correct move on to next question
+
+//else subtract 10 sec and then move to next question
+
+// if  the timer reaches zero GAME OVER
+
+// else if when all questions are answered GAME OVER
+
+// save the score and intials
+
+// Return to start
