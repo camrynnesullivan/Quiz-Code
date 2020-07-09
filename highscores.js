c0eefb8d2d58982f1a9591f4259cd48c6c6ef478
Camrynne Sullivan <camrynne.sullivan@gmail.com>
2020-07-08 22:36:05 -0400

    Added JS to highscores.js

---
+++ highscores.js
@@ -0,0 +1,35 @@
+// Starting Data
+
+var scoresList = document.getElementById("list");
+var userName = document.getElementById("user-name");
+var score = document.getElementById("user-score");
+var displayScores = document.getElementById("display-scores");
+
+// With help from classmates in the final hour - this is as far as we got
+
+// var retrievedScores = JSON.parse(localStorage.getItem("Initials & Score"));
+// scoresList.textContent = retrievedScores;
+// localStorage.setItem("Names and Scores", JSON.stringify(combo));
+// const prevScores = JSON.parse(localStorage.getItem("Names and Scores"));
+
+//User Input
+submitScore.addEventListener("click", function () {
+  console.log(userName.value);
+  console.log(score.value);
+  var fullInfo = {
+    userName: userName.value,
+    score: score.value,
+  };
+  var combo = userName.value + ": " + score.value;
+  var newScore = document.createElement("li");
+  var userInput = document.createTextNode(combo);
+  newScore.appendChild(userInput);
+  document.getElementById("list").appendChild(newScore);
+  console.log(fullInfo);
+  localStorage.setItem("User's Full Info", JSON.stringify(fullInfo));
+  var prevScores = JSON.parse(localStorage.getItem("User's Full Info"));
+  if (prevScores !== null) {
+    displayScores.textContent = combo;
+    console.log(combo);
+  }
+});
